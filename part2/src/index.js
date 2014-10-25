7fbf0ddfd5b7a5834c78c43adfee519bcbb22844
Ryan Morrissey <23maverick23@gmail.com>
2014-10-25 17:59:06 -0400

    Update index.js

--- part2/src/index.js
+++ part2/src/index.js
@@ -2,5 +2,20 @@ module.exports = {
 
     wordCounts: function(s) {
         // TODO: implement this function
+        var ws = s.split(" "),
+            _len = ws.length,
+            _re = /[\s\.\?\,\-\!]/gi,  // ignore " ".,-?!
+            _i = 0,
+            d = {},
+            l = [];
+
+        for (_i; _i < _len; _i++) {
+            var p = {},
+                w = ws[_i].replace(_re, "");
+            p[w] = w.length;
+            l.push(p);
+        }
+
+        return d['data'] = l;
     }
 }
