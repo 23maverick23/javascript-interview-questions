f72d4a71595353c0260a0ca9729df2a034993646
Ryan Morrissey <23maverick23@gmail.com>
2014-10-25 18:17:19 -0400

    Update index.js

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,5 +1,19 @@
 module.exports = {
     collapse: function(obj) {
         // TODO: implement this function
+        // return @obj: {@str: value}
+        var _len = obj.length,
+            _i = 0,
+            d = {};
+
+        for (_i; _i < _len; _i++) {
+            if (typeof obj[_i]['value'] === 'object') {
+                d[obj[_i]['tag']] = collapse(obj[_i]['value']);
+            } else {
+                d[obj[_i]['tag']] = obj[_i]['value'];
+            }
+        }
+
+        return d;
     }
 }
